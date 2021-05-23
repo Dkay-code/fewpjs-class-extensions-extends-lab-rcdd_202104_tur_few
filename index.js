a092e7a9e2f3fecc934b298e4754518b9874d51c
Derya Kaygisiz <derya.kaygisiz@outlook.com>
2021-05-23 18:31:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,4 +20,14 @@ class Triangle extends Polygon {
        return false;
      }
    }
+
+class Square extends Polygon {
+  get isValid(){
+    if(this.arr[0] === this.arr[1] === this.arr[2] === this.arr[3]) {
+      return true;
+      } else {
+      return false;
+      }
+    }
+  }
 }
