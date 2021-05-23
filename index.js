2c4c2859ee18b10cdbb778134304763f99901601
Derya Kaygisiz <derya.kaygisiz@outlook.com>
2021-05-23 18:27:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ get perimeter(){
   return this.arr.reduce((acc, curr) => acc + curr, 0);
 }
 }
+
+class Triangle extends Polygon {
+  get isValid(){
+    if(this.arr[0] + this.arr[1] > this.arr[2] && this.arr[1] + this.arr[2] > this.arr[0] && this.sides[0] + this.sides[2] > this.sides[1]) {
+        return true;
+      } else {
+       return false;
+     }
+   }
+  }
+}
